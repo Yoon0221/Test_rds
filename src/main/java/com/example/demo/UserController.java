bc2df52902aae9b0a20f794c882c592748f58d3b
Yoon0221 <108733746+Yoon0221@users.noreply.github.com>
2024-10-26 01:35:30 +0900

    dev : 같은 소리하네

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/yoonbest")
+@RequestMapping("/api/yoonbest/realbeauti")
 public class UserController {
 
     @Autowired
